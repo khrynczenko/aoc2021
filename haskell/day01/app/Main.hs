13d327629c75db3a5991a2c4791400a3bc8f625d
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-12-01 18:01:16 +0100

    add haskell solution to day 1

---
+++ haskell/day01/app/Main.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import System.IO as IO
+
+type DepthMeasurement = Word
+
+parseInputFile :: FilePath -> IO [Word]
+parseInputFile path = do
+    contents <- IO.readFile path
+    return $ map read $ lines contents
+
+countIncreases :: [DepthMeasurement] -> Word
+countIncreases [] = 0
+countIncreases [x] = 0
+countIncreases (x1:x2:xs) =
+    if x2 > x1
+    then 1 + recurse
+    else recurse
+  where
+    recurse = countIncreases (x2:xs)
+
+countIncreasesTriple :: [DepthMeasurement] -> Word
+countIncreasesTriple [] = 0
+countIncreasesTriple [x1,x2,x3] = 0
+countIncreasesTriple (x1:x2:x3:x4:xs) =
+    if x4 + x3 + x2 > x3 + x2 + x1
+    then 1 + recurse
+    else recurse
+  where
+    recurse = countIncreasesTriple (x2:x3:x4:xs)
+countIncreasesTriple xs = 0
+
+main :: IO ()
+main = do
+    measurements <- parseInputFile "input.txt"
+    let depthIncreasesCount = countIncreases measurements
+    putStrLn $ "Answer 1: " <> show depthIncreasesCount
+    let depthIncreasesTripleCount = countIncreasesTriple measurements
+    putStrLn $ "Answer 2: " <> show depthIncreasesTripleCount
